2c17103480c43b1960d8b631f2521d6dbead2585
Binay Tiwari <109269213+Binnaries20@users.noreply.github.com>
2024-09-06 21:35:21 +0530

    Time: 2 ms (50.01%), Space: 7.5 MB (70.61%) - LeetHub

---
+++ 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int a=n;
+        int mask=0;
+        if(a==0){
+            return 1;
+        }
+        while(a!=0){
+            mask = (mask << 1) | 1;
+            a=a>>1;
+        }
+        return ((~n) & mask);
+    }
+};
